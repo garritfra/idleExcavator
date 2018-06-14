e32b243a285e0f71edcaf1aeeb4296b91073527d
garritfra <garritfranke@gmail.com>
2018-06-14 15:03:21 +0200

    use bigdouble for count

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import { Button } from "reactstrap";
+import BigDouble from "../model/BigDouble";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.props = props;
-    this.state = { dirt: 10 };
+    this.state = { dirt: new BigDouble(2, 0) };
+    console.log(this.state);
   }
 
   handleClick() {
-    this.setState({ dirt: this.state.dirt + 1 });
+    this.setState({ dirt: this.state.dirt.add(1) });
   }
 
   render() {
@@ -17,7 +19,7 @@ export default class App extends Component {
       <div>
         <span>
           <Button onClick={this.handleClick.bind(this)}>Click Me!</Button>
-          <h1>{this.state.dirt}</h1>
+          <h1>{this.state.dirt.toString()}</h1>
         </span>
       </div>
     );
