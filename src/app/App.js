5aed5c0b1f992cc2c99fa62ac0cc2dbdf7e251cc
Fade97 <Fade97@users.noreply.github.com>
2018-06-14 15:29:04 +0200

    Fix

--- src/app/App.js
+++ src/app/App.js
@@ -3,15 +3,18 @@ import { Button } from "reactstrap";
 import BigDouble from "../model/BigDouble";
 
 export default class App extends Component {
+
   constructor(props) {
     super(props);
+    this.d1 = new BigDouble(1,0);
     this.props = props;
     this.state = { dirt: new BigDouble(2, 0) };
     console.log(this.state);
   }
 
   handleClick() {
-    this.setState({ dirt: this.state.dirt.add(1) });
+    this.d1.addCount(2,0);
+    this.setState({ dirt: 1 });
   }
 
   render() {
@@ -19,7 +22,7 @@ export default class App extends Component {
       <div>
         <span>
           <Button onClick={this.handleClick.bind(this)}>Click Me!</Button>
-          <h1>{this.state.dirt.toString()}</h1>
+          <h1>{this.d1.toString()}</h1>
         </span>
       </div>
     );
