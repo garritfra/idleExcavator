2fb92dc8e8692141ba6fca6bb58898c6a7cf17f9
garritfra <garritfranke@gmail.com>
2018-06-14 14:49:12 +0200

    add counter

--- src/app.js
+++ src/app.js
@@ -2,7 +2,25 @@ import React, { Component } from "react";
 import { Button } from "reactstrap";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = { dirt: 10 };
+  }
+
+  handleClick() {
+    const newDirt = this.state.dirt + 1;
+    this.setState({ dirt: newDirt });
+  }
+
   render() {
-    return <Button>Hello World</Button>;
+    return (
+      <div>
+        <span>
+          <Button onClick={this.handleClick.bind(this)}>Click Me!</Button>
+          <h1>{this.state.dirt}</h1>
+        </span>
+      </div>
+    );
   }
 }
