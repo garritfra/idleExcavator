2a1bf701a8ebe5646de44f0ae3e642d682595907
Fade97 <Fade97@users.noreply.github.com>
2018-06-14 13:40:32 +0200

    Add BigDouble subtract

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import BigDouble from "model/BigDouble"
 
 ReactDOM.render(<h1>Hello World</h1>, Document.getElementById("root"));
+
+
+let d1 = new BigDouble(1.25223, 24);
+console.log(d1.toString());
--- src/model/BigDouble.js
+++ src/model/BigDouble.js
@@ -1,3 +1,5 @@
+const precision = 7
+
 class BigDouble {
     constructor(mantissa, exponent) {
         this.mantissa = mantissa;
@@ -12,14 +14,15 @@ class BigDouble {
     add(other) {
         if ( typeof(other) != BigDouble) {
             console.log("[BigDouble | add] Argument must be of type BigDouble");
+            return false;
         }
 
-        if ( this.exponent - other.exponent > 0 && this.exponent - other.exponent < 7 ) {
-            this.mantissa += other.exponent / Math.pow(10, this.exponent - other.exponent);
+        if ( this.exponent - other.exponent > 0 && this.exponent - other.exponent < precision ) {
+            this.mantissa += other.mantissa / Math.pow(10, this.exponent - other.exponent);
         }
         else if ( this.exponent - other.exponent < 0 ) {
             this.exponent = other.exponent;
-            if ( this.exponent - other.exponent > -7 ) {
+            if ( this.exponent - other.exponent > -precision ) {
                 this.mantissa = this.mantissa / Math.pow(10, other.exponent - this.exponent);
             }
             else {
@@ -29,11 +32,47 @@ class BigDouble {
         else if ( this.exponent - other.exponent === 0) {
             this.mantissa += other.mantissa;
         }
+        else {
+            console.log("[BigDouble | add] Could not add " + other);
+        }
 
         while ( this.mantissa >= 10 ) {
             this.exponent += 1;
             this.mantissa -= 10;
         }
+        return true;
+    }
+
+    subtract(other) {
+        if ( typeof(other) != BigDouble) {
+            console.log("[BigDouble | subtract] Argument must be of type BigDouble");
+            return false;
+        }
+
+        if ( this.exponent - other.exponent > 0 && this.exponent - other.exponent < precision ) {
+            this.mantissa -= other.mantissa / Math.pow(10, this.exponent - other.exponent);
+        }
+        else if ( this.exponent - other.exponent < 0 ) {
+            return false;
+        }
+        else if ( this.exponent - other.exponent === 0) {
+            if ( this.mantissa >= other.mantissa ) {
+                this.mantissa -= other.mantissa;
+            }
+        }
+        else {
+            console.log("[BigDouble | subtract] Could not subtract " + other);
+        }
+
+        while ( this.mantissa < 0 ) {
+            this.exponent -= 1;
+            this.mantissa *= 10;
+        }
+        return true;
+    }
+
+    toString() {
+        return this.mantissa + "e" + this.exponent;
     }
 
 }
