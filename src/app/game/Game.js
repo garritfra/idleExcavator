1b84a079d6c45efb24f1bc1689de2f1c6a61790f
garritfra <garritfranke@gmail.com>
2018-06-16 23:17:12 +0200

    linting

--- src/app/game/Game.js
+++ src/app/game/Game.js
@@ -6,15 +6,15 @@ let instance = null;
 export default class Game {
   constructor(tickRate) {
     this.dirt = new BigDouble(1, 0);
-    this.autoPercentage = new BigDouble(1,0);
+    this.autoPercentage = new BigDouble(1, 0);
     console.log(this.autoPercentage);
     console.log(this.dirt);
     this.clickerPercentage = 1.0;
     this.tickRate = tickRate;
     this.tick = this.tick.bind(this);
     this.upgradesList = [
-      new AutoUpgrade("worker", new BigDouble(1,1), new BigDouble(0.1,0)),
-      new AutoUpgrade("wheelbarrow", new BigDouble(1,2), new BigDouble(0.5,0))
+      new AutoUpgrade("worker", new BigDouble(1, 1), new BigDouble(0.1, 0)),
+      new AutoUpgrade("wheelbarrow", new BigDouble(1, 2), new BigDouble(0.5, 0))
     ];
   }
 
@@ -51,11 +51,18 @@ export default class Game {
   }
 
   applyAutoUpgrade(upgrade) {
-    if ( this.dirt.subtract(upgrade.cost) )
-    {
+    if (this.dirt.subtract(upgrade.cost)) {
       upgrade.buy();
-      this.autoPercentage.add(this.upgradesList[0].getPercentage().getTimes(this.upgradesList[0].amountBought,0));
-      this.autoPercentage.add(this.upgradesList[1].getPercentage().getTimes(this.upgradesList[1].amountBought,0));
+      this.autoPercentage.add(
+        this.upgradesList[0]
+          .getPercentage()
+          .getTimes(this.upgradesList[0].amountBought, 0)
+      );
+      this.autoPercentage.add(
+        this.upgradesList[1]
+          .getPercentage()
+          .getTimes(this.upgradesList[1].amountBought, 0)
+      );
     }
   }
 }
