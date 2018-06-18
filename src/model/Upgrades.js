d996a6aad5b50c4d6ec5ec235db763a99f9541c2
garritfra <garritfranke@gmail.com>
2018-06-18 14:54:08 +0200

    rename vacuum to vacuum cleaner

--- src/model/Upgrades.js
+++ src/model/Upgrades.js
@@ -3,6 +3,6 @@ import AutoUpgrade from "./AutoUpgrade";
 
 exports.AutoUpgrades = [
   new AutoUpgrade("Worker", new BigDouble(1, 1), new BigDouble(0.1, 0)),
-  new AutoUpgrade("Vacuum", new BigDouble(1, 3), new BigDouble(1, 0)),
+  new AutoUpgrade("Vacuum Cleaner", new BigDouble(1, 3), new BigDouble(1, 0)),
   new AutoUpgrade("Wheelbarrow", new BigDouble(1, 5), new BigDouble(0.5, 0))
 ];
