6ed2f568cedf2ca1008471f863091cf2c9bc4a9b
garritfra <garritfranke@gmail.com>
2018-06-19 16:29:44 +0200

    typescriptify Upgrades

--- src/model/Upgrades.ts
+++ src/model/Upgrades.ts
@@ -1,7 +1,7 @@
 import BigDouble from "./BigDouble";
 import AutoUpgrade from "./AutoUpgrade";
 
-exports.AutoUpgrades = [
+export const AutoUpgrades: Array<AutoUpgrade> = [
   new AutoUpgrade("Worker", new BigDouble(1, 1), new BigDouble(1, -1)),
   new AutoUpgrade("Vacuum Cleaner", new BigDouble(1.5, 2), new BigDouble(1, 0)),
   new AutoUpgrade("Wheelbarrow", new BigDouble(2, 3), new BigDouble(1.5, 1))
